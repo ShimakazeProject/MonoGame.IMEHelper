58f77f313c9e75e76618c538c990138d7f1a8660
舰队的偶像-岛风酱! <frg2089@outlook.com>
2024-03-10 14:26:59 +0800

    update
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

--- build.ps1
+++ build.ps1
@@ -3,16 +3,20 @@ if ($null -EQ $IsWindows -AND 'Desktop' -EQ $PSEdition ) {
     $Script:IsWindows = $true
 }
 
-dotnet pack --graph --configuration 'UniversalGLRelease'
-if ($IsWindows) {
-    @('WindowsDXRelease', 'WindowsGLRelease', 'WindowsXNARelease') | ForEach-Object {
-        $Configuration = $PSItem
-        $Platform = 'Any CPU'
-        if ($Configuration -EQ 'WindowsXNARelease') {
-            $Platform = 'x86'
+@('Debug', 'Release') | ForEach-Object {
+    $ConfigurationSuffix = $PSItem
+    dotnet build --graph --configuration "UniversalGL$ConfigurationSuffix"
+    dotnet pack --graph --configuration "UniversalGL$ConfigurationSuffix"
+    if ($IsWindows) {
+        @("WindowsDX$ConfigurationSuffix", "WindowsGL$ConfigurationSuffix", "WindowsXNA$ConfigurationSuffix") | ForEach-Object {
+            $Configuration = $PSItem
+            $Platform = 'Any CPU'
+            if ($Configuration -EQ "WindowsXNA$ConfigurationSuffix") {
+                $Platform = 'x86'
+            }
+    
+            dotnet build --graph --configuration $Configuration -property:Platform=$Platform
+            dotnet pack --graph --configuration $Configuration -property:Platform=$Platform
         }
-
-        dotnet build --graph --configuration $Configuration -property:Platform=$Platform
-        dotnet pack --graph --configuration $Configuration -property:Platform=$Platform
     }
 }
